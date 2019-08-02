65a4471dc874f7e368980b646c303bdaa0c12dce
Will Hodgson <will.hodgson@current-rms.com>
2019-08-02 09:02:22 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+end
